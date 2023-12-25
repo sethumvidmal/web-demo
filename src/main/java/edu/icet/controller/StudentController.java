0b111d6871680a8de2d61036895c966a941e9944
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-25 11:55:19 +0530

    Adding descriptions for APIs

--- src/main/java/edu/icet/controller/StudentController.java
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -3,30 +3,39 @@ package edu.icet.controller;
 import edu.icet.dto.StudentDTO;
 import edu.icet.entity.StudentEntity;
 import edu.icet.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@Tag(name = "Student information")
 @RequestMapping("/student")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
+    @Operation(description = "Get all students")
     @GetMapping("/get-all-students")
     public Iterable<StudentEntity> getStudent() {
         return studentService.getStudent();
     }
+
+    @Operation(description = "Get number of students")
     @GetMapping("/get-student-count")
-    public int getStudentCount(){
+    public int getStudentCount() {
         return studentService.getStudentCount();
     }
+
+    @Operation(description = "Get existing student by first name")
     @GetMapping("/{firstName}")
     public Iterable<StudentEntity> getStudentByFirstName(String name) {
         return studentService.getStudentByFirstName(name);
     }
 
+    @Operation(description = "Create Student in CRM System")
     @PostMapping
     public void createStudent(@RequestBody StudentDTO studentDTO) {
         studentService.createStudent(studentDTO);
